01b72ac12dfc5b6538b2f2326a676796dbdc442b
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-07-29 00:30:28 +0900

    CHORE : file api swagger 주석 추가

--- src/main/java/com/fledge/fledgeserver/file/dto/PresignedUrlResponse.java
+++ src/main/java/com/fledge/fledgeserver/file/dto/PresignedUrlResponse.java
@@ -1,11 +1,17 @@
 package com.fledge.fledgeserver.file.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@Schema(description = "프리사인드 URL 응답 DTO")
 public class PresignedUrlResponse {
+
+    @Schema(description = "생성된 프리사인드 URL", example = "https://example.com/presigned-url")
     private final String url;
+
+    @Schema(description = "파일 경로", example = "images/example.txt")
     private final String filePath;
 }
