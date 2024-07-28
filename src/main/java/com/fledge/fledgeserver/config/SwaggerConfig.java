69df2e5a8f9fd249258dcde4d878c7fd8485207f
gaguriee <74501631+gaguriee@users.noreply.github.com>
2024-07-29 02:02:06 +0900

    FIX : Swagger Failed to fetch error

--- src/main/java/com/fledge/fledgeserver/config/SwaggerConfig.java
+++ src/main/java/com/fledge/fledgeserver/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,7 +33,7 @@ public class SwaggerConfig {
                         .scheme(BEARER_TOKEN_PREFIX)
                         .bearerFormat(securityJwtName));
 
-        return new OpenAPI()
+        return new OpenAPI().addServersItem(new Server().url("/"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
