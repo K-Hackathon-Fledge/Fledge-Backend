3d1e3153683ada306d9afabbc3b49e026b408b2a
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-08-05 22:15:02 +0900

    REFACTORING : SupportController Public API 분리

--- src/main/java/com/fledge/fledgeserver/support/controller/SupportController.java
+++ src/main/java/com/fledge/fledgeserver/support/controller/SupportController.java
@@ -48,16 +48,6 @@ public class SupportController {
         return ApiResponse.success(CREATE_SUPPORT_SUCCESS);
     }
 
-    @Operation(summary = "후원하기 게시글 조회",
-            description = "후원하기 게시글을 조회합니다.(모든 회원 가능)")
-    @GetMapping("/{supportId}")
-    public ResponseEntity<ApiResponse<PostGetResponse>> getSupport(
-            @PathVariable(value = "supportId") Long supportId
-    ) {
-        // TODO :: 후원 인증 관련 로직 추가
-        return ApiResponse.success(GET_SUPPORT_SUCCESS, supportService.getSupport(supportId));
-    }
-
     @Operation(summary = "후원 물품 금액 후원하기",
             description = "후원하기 게시글에서 후원 물품 금액 조회합니다.(모든 회원 가능)\n" +
                     "\n" +
@@ -73,14 +63,7 @@ public class SupportController {
         return ApiResponse.success(CREATE_DONATE_SUCCESS);
     }
 
-    @Operation(summary = "후원 진행률",
-            description = "후원하기 게시글 및 후원하기 시에 후원 진행률 반환")
-    @GetMapping("/{supportId}/progress")
-    public ResponseEntity<ApiResponse<RecordProgressGetResponse>> getSupportProgress(
-            @PathVariable(value = "supportId") Long supportId
-    ) {
-        return ApiResponse.success(GET_SUPPORT_PROGRESS_SUCCESS, supportService.getSupportProgress(supportId));
-    }
+
 
     @Operation(summary = "후원하기 게시글 수정 시 기존 데이터 조회",
             description = "후원하기 게시글의 기존 데이터를 반환합니다.\n" +
@@ -114,40 +97,6 @@ public class SupportController {
         supportService.updateSupportPost(memberId, supportId, postUpdateRequest);
         return ApiResponse.success(SuccessStatus.UPDATE_SUPPORT_SUCCESS);
     }
+
     // TODO :: 후원하기 게시글 삭제 API
-    @Operation(summary = "후원하기 게시글 리스트 페이징",
-            description = "후원하기 게시글을 페이징하여 조회합니다.\n" +
-                    "\n" +
-                    "### 요청 파라미터\n" +
-                    "- **page**: 현재 페이지 번호 (기본값: 1)\n" +
-                    "- **q**: 검색어 (제목, 내용 기준으로 검색)\n" +
-                    "- **category**: 카테고리 (여러 개 선택 가능)\n" +
-                    "  - 사용 예: category=\"Food\"&category=\"MEDICAL\"\n" +
-                    "- **status**: 게시글 상태 ('ing'/'end', 기본값: 'ing')\n" +
-                    "\n" +
-                    "### 가능한 카테고리\n" +
-                    "[DAILY_NECESSITY, FOOD, HOME_APPLIANCES, EDUCATION, MEDICAL, LEGAL_AID, ETC]")
-    @GetMapping("/paging")
-    public ResponseEntity<ApiResponse<PostTotalPagingResponse>> pagingSupportPost(
-            @RequestParam(defaultValue = "1") int page, // 현재 페이지
-//            @RequestParam(defaultValue = "9") int limit //무조건 9개
-            @RequestParam(defaultValue = "") String q,
-            @RequestParam(defaultValue = "") List<String> category, // 카테고리
-            @RequestParam(defaultValue = "ing") String status
-    ) {
-        // 응답에 이미지 포함 시키기
-        return ApiResponse.success(GET_SUPPORT_POST_PAGING_SUCCESS, supportService.pagingSupportPost(page-1, q, category, status));
-    }
-
-    @Operation(summary = "마감 임박한 후원하기 게시글",
-            description = "4개씩 D-7까지 (limit=4, leftDays=7)")
-    @GetMapping("/deadline")
-    public ResponseEntity<ApiResponse<PostTotalPagingResponse>> deadlineApproachingPosts(
-            @RequestParam(defaultValue = "1") int page
-//            @RequestParam(defaultValue = "10") int limit // 무조건 4개
-    ) {
-        return ApiResponse.success(GET_DEADLINE_APPROACHING_POST_SUCCESS, supportService.deadlineApproachingPosts(page-1));
-    }
-
-
 }
