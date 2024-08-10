14ebda7ee8b5fd274b67b6298b78259da0801745
JuseungL <121665437+JuseungL@users.noreply.github.com>
2024-08-10 16:08:52 +0900

    REFACTORING : 후원률 소숫점 한자리 버림

--- src/main/java/com/fledge/fledgeserver/support/dto/response/RecordProgressGetResponse.java
+++ src/main/java/com/fledge/fledgeserver/support/dto/response/RecordProgressGetResponse.java
@@ -2,10 +2,13 @@ package com.fledge.fledgeserver.support.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import java.text.DecimalFormat;
 
 @Getter
 @Schema(description = "후원 게시글 조회 시 후원 기록 반환")
 public class RecordProgressGetResponse {
+
+    private static final DecimalFormat df = new DecimalFormat("#.#");
 
     @Schema(description = "후원 물품 총 금액", example = "1000000")
     private int totalPrice;
@@ -16,11 +19,14 @@ public class RecordProgressGetResponse {
     @Schema(description = "후원 진행률(%)", example = "40")
     private double progress;
 
-
     public RecordProgressGetResponse(int totalPrice, int supportedPrice) {
         this.totalPrice = totalPrice;
         this.supportedPrice = supportedPrice;
-        double progressValue = (double) supportedPrice / totalPrice * 100;
-        this.progress = progressValue;
+        if (totalPrice == 0) {
+            this.progress = 0.0;
+        } else {
+            double progressValue = (double) supportedPrice / totalPrice * 100;
+            this.progress = Double.parseDouble(df.format(progressValue));
+        }
     }
 }
